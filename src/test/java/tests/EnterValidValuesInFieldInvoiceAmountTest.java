3f9b17a1c1a2a7086a341060af83310aeb9e62b8
gaevskiy94 <gaevskiy.alexandr@gmail.com>
2024-04-01 17:45:57 +0300

    Updated tests the enter valid values in field invoice amount. Added test the enter numeric with two decimal places in field invoice amount.

--- src/test/java/tests/EnterValidValuesInFieldInvoiceAmountTest.java
+++ src/test/java/tests/EnterValidValuesInFieldInvoiceAmountTest.java
@@ -7,29 +7,40 @@ import pages.FactoringCalculatorPage;
 
 public class EnterValidValuesInFieldInvoiceAmountTest extends BaseTest{
 
-    @Test(priority = 1)
+    @Test()
     @Parameters({"minValueForFieldInvoiceAmount"})
     public void EnterMinValueInFieldInvoiceAmountTest(String minValueForFieldInvoiceAmount){
         FactoringCalculatorPage factoringCalculatorPage = new FactoringCalculatorPage();
 
         factoringCalculatorPage.inputValueInFieldInvoiceAmount(minValueForFieldInvoiceAmount);
-        factoringCalculatorPage.CheckNoErrorMessageUnderFieldInvoiceAmount();
+        factoringCalculatorPage.checkNoErrorMessageUnderFieldInvoiceAmount();
 
         factoringCalculatorPage.clickButtonCalculate();
-
         Assert.assertTrue(factoringCalculatorPage.isCalculationMade());
     }
 
-    @Test(priority = 2)
+    @Test()
+    @Parameters({"numericTwoDecimalPlacesForFieldInvoiceAmount"})
+    public void EnterNumericTwoDecimalPlacesInFieldInvoiceAmountTest(
+            String numericTwoDecimalPlacesForFieldInvoiceAmount){
+        FactoringCalculatorPage factoringCalculatorPage = new FactoringCalculatorPage();
+
+        factoringCalculatorPage.inputValueInFieldInvoiceAmount(numericTwoDecimalPlacesForFieldInvoiceAmount);
+        factoringCalculatorPage.checkNoErrorMessageUnderFieldInvoiceAmount();
+
+        factoringCalculatorPage.clickButtonCalculate();
+        Assert.assertTrue(factoringCalculatorPage.isCalculationMade());
+    }
+
+    @Test()
     @Parameters({"maxValueForFieldInvoiceAmount"})
     public void EnterMaxValueInFieldInvoiceAmountTest(String maxValueForFieldInvoiceAmount){
         FactoringCalculatorPage factoringCalculatorPage = new FactoringCalculatorPage();
 
         factoringCalculatorPage.inputValueInFieldInvoiceAmount(maxValueForFieldInvoiceAmount);
-        factoringCalculatorPage.CheckNoErrorMessageUnderFieldInvoiceAmount();
+        factoringCalculatorPage.checkNoErrorMessageUnderFieldInvoiceAmount();
 
         factoringCalculatorPage.clickButtonCalculate();
-
         Assert.assertTrue(factoringCalculatorPage.isCalculationMade());
     }
 }
